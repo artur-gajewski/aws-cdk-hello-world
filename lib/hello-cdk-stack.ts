051deb3ec8e57758d2d675cd4920c86b944de7c3
Artur Gajewski <aga@Arturs-MacBook-Pro.local>
2026-03-04 12:25:39 +0200

    Make lambda return actual data

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -15,9 +15,23 @@ export class HelloCdkStack extends cdk.Stack {
       handler: "index.handler",
       code: lambda.Code.fromInline(`
         exports.handler = async function(event) {
+          const now = new Date();
+
+          const dd = now.getDate();
+          const mm = now.getMonth() + 1;
+          const yyyy = now.getFullYear();
+          const date = dd + '.' + mm + '.' + yyyy;
+
+          // ISO week number calculation
+          const d = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
+          const dayNum = d.getUTCDay() || 7;
+          d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+          const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+          const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+
           return {
             statusCode: 200,
-            body: JSON.stringify('Hello World!'),
+            body: JSON.stringify({ date: date, week: weekNo }),
           };
         };
       `),
